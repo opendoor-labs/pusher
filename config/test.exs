9bafae398e286eb0efe2b2510afd223a3d9fedec
Nolan Evans <nolane@gmail.com>
2015-11-07 11:00:35 -0800

    test server only to listen on loopback

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :pusher, Pusher.Endpoint,
-  http: [port: 4001],
+  http: [ip: {127,0,0,1}, port: 4001],
   pubsub: [adapter: Phoenix.PubSub.Redis],
   server: false
 
