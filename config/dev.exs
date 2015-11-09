20fc42a64fd18bb496526a92c76845843e35c05f
Sarah & Nolan <github+sarah+nolan@opendoor.com>
2015-11-09 11:34:17 -0800

    remove comments

--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,5 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
 config :pusher, Pusher.Endpoint,
   http: [ip: {127,0,0,1}, port: System.get_env("PORT") || 4000],
   pubsub: [adapter: Phoenix.PubSub.Redis],
