374b7524e077e65c6ff7a362f0987502a219f443
Nick Green <nick.green@opendoor.com>
2017-02-07 12:12:29 -0800

    Now with named pubsub

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :pusher, Pusher.Endpoint,
   http: [port: {:system, "PORT"}],
-  pubsub: [adapter: Phoenix.PubSub.Redis, url: System.get_env("REDIS_URL")],
+  pubsub: [name: Pusher.Pubsub, adapter: Phoenix.PubSub.Redis, url: System.get_env("REDIS_URL")],
   url: [host: "opendoor-pusher.herokuapp.com"],
   check_origin: ["//www.opendoor.com"],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
